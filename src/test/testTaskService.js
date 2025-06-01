8aee3617804e373e578250b91b6b9eb572af7dd8
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-01 19:27:44 -0300

    feat: Adicionando diretório para testes simples

---
+++ src/test/testTaskService.js
@@ -0,0 +1,23 @@
+// src/test/testTaskService.js
+import 'dotenv/config';
+import db from '../models/index.js';
+import { findTaskByIdForUser } from '../services/taskService.js';
+
+const test = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log('✅ Conectado ao banco com sucesso.');
+
+    const userId = 2;
+    const taskId = 2;
+
+    const task = await findTaskByIdForUser(taskId, userId);
+    console.log('🎯 Tarefa encontrada:', task.toJSON());
+  } catch (error) {
+    console.error('❌ Erro:', error.message);
+  } finally {
+    await db.sequelize.close();
+  }
+};
+
+test();
