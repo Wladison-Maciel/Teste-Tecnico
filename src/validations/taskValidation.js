10a7739249a5c8607cec99521b0162e566fbe771
Wladison-Maciel <franciscowladison97@gmail.com>
2025-06-02 10:19:31 -0300

    feat: Adicionando método para gerar uma resposta 400 para campos não reconhecidos no método PATCH

--- src/validations/taskValidation.js
+++ src/validations/taskValidation.js
@@ -1,15 +1,22 @@
 import { z } from 'zod';
 
+// Schema para criar tarefa
 export const taskCreateSchema = z.object({
   title: z.string().min(1, 'Título é obrigatório'),
   description: z.string().optional(),
   status: z.enum(['PENDING', 'COMPLETED']).optional(),
-  dueDate: z.string().refine(date => !isNaN(Date.parse(date)), 'Data inválida'),
-});
+  dueDate: z
+    .string()
+    .refine(date => !isNaN(Date.parse(date)), 'Data inválida'),
+}).strict();
 
+// Schema para atualizar tarefa
 export const taskUpdateSchema = z.object({
-  title: z.string().optional(),
+  title: z.string().min(1, 'Título inválido').optional(),
   description: z.string().optional(),
   status: z.enum(['PENDING', 'COMPLETED']).optional(),
-  dueDate: z.string().optional(),
-});
+  dueDate: z
+    .string()
+    .refine(date => !isNaN(Date.parse(date)), 'Data inválida')
+    .optional(),
+}).strict();
